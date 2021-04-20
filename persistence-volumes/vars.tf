b418922a6e40dc62fd7fb6edbac86e420d20be90
Alejandro Cardenas <alecar.main@gmail.com>
2021-04-20 06:50:13 -0500

    Change type of instances tags

--- persistence-volumes/vars.tf
+++ persistence-volumes/vars.tf
@@ -21,7 +21,7 @@ variable "instances" {
   type = object(
     {
       ids = list(string)
-      tags = list(list(string))
+      tags = list(string)
       availability_zone = list(string)
     }
   )
